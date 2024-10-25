59d02440c733b2758f482310855db21d54266f6f
Victor Carbelotti <victorcarbelotti0306@gmail.com>
2024-10-25 14:06:31 -0300

    update Carlos 24/10

--- src/pages/Data/dataCarlos.js
+++ src/pages/Data/dataCarlos.js
@@ -1,1 +1,6 @@
-export const dataCadastrador2 = [];
+export const dataCadastrador2 = [    
+    { produto: '39931 - Alicate Anéis Externo Curvo 7" Gedore Red', data: '2024-10-22' },
+    { produto: '39930 - Alicate Anéis Externo Reto 7" Gedore Red', data: '2024-10-23' },
+    { produto: '39933 - Alicate Anéis Interno Reto 7" Gedore Red', data: '2024-10-23' },
+    { produto: '30919 - Alicate para Anéis Interno Reto 7" Gedore', data: '2024-10-24' },
+];
